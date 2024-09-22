092c80f219e954a0a73dbb35d938ec1c2ac2c568
francisgobbi <francis.gobbi@db.tec.br>
2024-09-22 04:18:58 -0300

    update json

--- ex08.js
+++ ex08.js
@@ -6,7 +6,7 @@ import { sleep } from 'k6'
 export const options = {
   scenarios: {
       navegador: {
-        executor: 'per-vu-',
+        executor: 'per-vu-iterations',
         vus: 1,
         iterations: 1,
         options: {
@@ -21,9 +21,10 @@ export const options = {
 }
 
 export default async function() { 
-    const navegador = await browser.newPage();
+
     
-    await navegador.goto('http://165.227.93.41/lojinha-web/v2/')
+    const navegador = await browser.newPage();    
+    await navegador.goto('http://165.227.93.41/lojinha-web/v2/')   
     await navegador.fill('#usuario','admin')
     await navegador.fill('#senha','admin')
     await navegador.screenshot({path: 'evidencias/1.png'})
